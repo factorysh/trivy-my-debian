7c3b25d5dec4423d31b11a88ffeca77b2b90abdb
Mathieu Lecarme <mlecarme@bearstech.com>
2021-10-26 16:12:11 +0200

    More stuff

--- debian.py
+++ debian.py
@@ -2,36 +2,52 @@ import ijson
 import json
 
 
+DEBIANS = {"12": "bookworm", "11": "bullseye", "10": "buster", "9": "stretch"}
+
+
 class DB:
     def __init__(self, path: str):
-        self.f = open(path, 'r')
+        self.f = open(path, "r")
 
     def cve(self, name):
         self.f.seek(0)
-        for k, v in ijson.kvitems(self.f, ''):
+        for k, v in ijson.kvitems(self.f, ""):
             if name in v:
                 return k, v[name]
 
 
 class TrivyDebian:
     def __init__(self, data):
+        if data["Metadata"]["OS"]["Family"] != "debian":
+            raise Exception("Not a Debian")
         self.data = data
+        self._n = None
+
+    def name(self):
+        if self._n is None:
+            self._n = self.data["Metadata"]["OS"]["Name"].split(".")[0]
+        return DEBIANS[self._n]
 
     def cve(self):
-        for result in self.data['Results']:
-            if result['Type'] != 'debian':
+        for result in self.data["Results"]:
+            if result["Type"] != "debian":
                 continue
-            for vulnerability in result['Vulnerabilities']:
-                yield vulnerability['VulnerabilityID']
-
-
-
+            for vulnerability in result["Vulnerabilities"]:
+                yield vulnerability
 
 
 if __name__ == "__main__":
     import os, sys
-    d = DB(os.getenv('DB'))
+    from pprint import pprint
+
+    d = DB(os.getenv("DB"))
     td = TrivyDebian(json.load(sys.stdin))
+    name = td.name()
     for cve in td.cve():
-        dcve = d.cve(cve)
-        print(cve, dcve)
+        package, info = d.cve(cve["VulnerabilityID"])
+        pprint(cve)
+        if name in info["releases"]:
+            pprint(info["releases"][name])
+        else:
+            pprint(info["releases"])
+        print("\n\n")
