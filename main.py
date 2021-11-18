9f3c7deabe2a0f0408df45f41fc1077148f409f5
Mathieu Lecarme <mlecarme@bearstech.com>
2021-11-18 18:00:11 +0100

    More pedantic, more POST.

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
+from typing import Optional, Dict, List
+import logging
+import os
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from debian import DB, TrivyDebian, Trivy
+
+
+td = TrivyDebian(
+    DB(os.getenv("DB"), "./debian.db"),
+    not_package=["vim", "systemd", "rsyslog"],
+    not_severity=["LOW"],
+    debian_minor=False,
+)
+td.logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+app = FastAPI(on_startup=[])
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
+@app.post("/trivy/debian")
+async def debian(audit: Trivy):
+    return [cve for cve, package, info, ticket in td.scan(audit)]
